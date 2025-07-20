b1a43b065b59c04610a89a976ce4fe3fc6b9755b
cursed144 <107793642+cursed144@users.noreply.github.com>
2025-07-20 14:10:43 +0300

    Add rocket movement
    Implemented basic rocket movement controls in rocket.gd

--- Game/Scripts/rocket.gd
+++ Game/Scripts/rocket.gd
@@ -1,1 +1,26 @@
 extends RigidBody2D
+
+var ROT_SPEED: float = 3250
+var ACCEL_SPEED := 500 * Vector2.UP
+var DECCEL_SPEED: float = 60
+var ROT_DECCEL: float = 1.5
+
+
+func _physics_process(delta: float) -> void:
+	process_basic_movement(delta)
+	print(angular_velocity)
+
+
+
+func process_basic_movement(delta) -> void:
+	if Input.is_action_pressed("forward"):
+		apply_central_force(ACCEL_SPEED.rotated(rotation) * delta)
+	elif Input.is_action_pressed("slow_down"):
+		linear_velocity = linear_velocity.move_toward(Vector2.ZERO, DECCEL_SPEED * delta)
+	
+	if Input.is_action_pressed("rot_right") and Input.is_action_pressed("rot_left"):
+		angular_velocity = move_toward(angular_velocity, 0, ROT_DECCEL * delta)
+	elif Input.is_action_pressed("rot_right"):
+		apply_torque(ROT_SPEED * delta)
+	elif Input.is_action_pressed("rot_left"):
+		apply_torque(-ROT_SPEED * delta)
