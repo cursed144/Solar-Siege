d92120d6540bf4afe802fd3a86ff3a6eae2ca603
cursed144 <107793642+cursed144@users.noreply.github.com>
2025-11-06 22:50:42 +0200

    Add building confirmation menu
    Introduces a confirmation menu UI for building construction. Not functional yet

--- Game/Planet-Section/Scripts/building_menu.gd
+++ Game/Planet-Section/Scripts/building_menu.gd
@@ -4,8 +4,19 @@ extends Control
 func _ready() -> void:
 	for section in $Buildings.get_children():
 		for building in section.get_children():
-			building.open_confirmation_menu.connect(_open_confirmation_menu)
+			building.open_confirmation_menu.connect(open_confirmation_menu)
 
 
-func _open_confirmation_menu(data: BuildingData) -> void:
-	print("open")
+func open_confirmation_menu(data: BuildingData) -> void:
+	$ConfirmMenu/Icon.texture = data.icon
+	$ConfirmMenu/Title.text = data.display_name
+	$ConfirmMenu/Desc.text = data.description
+	$ConfirmMenu.show()
+
+
+func on_confirm_menu_cancelled() -> void:
+	$ConfirmMenu.hide()
+
+
+func on_building_confirmed() -> void:
+	pass
--- Game/Planet-Section/building_data.gd
+++ Game/Planet-Section/building_data.gd
@@ -5,6 +5,7 @@ extends Resource
 @export var build_time: float
 @export var clearance: Vector2
 @export var display_name: String
+@export var description: String
 @export var cost: int = 0
 @export var icon: Texture2D
 @export var scene: PackedScene
