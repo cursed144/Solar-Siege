a8ede8afe78b74eb1249be8923b5de017c647170
cursed144 <107793642+cursed144@users.noreply.github.com>
2025-12-18 00:26:18 +0200

    Make building inventory names configurable via export

--- Game/Planet-Section/Classes/building.gd
+++ Game/Planet-Section/Classes/building.gd
@@ -3,20 +3,25 @@ extends Area2D
 
 signal request_inv_update(inv_name: String)
 
-@export var inventories: Dictionary[String, Inventory]
+@export_category("Workers")
 @export var max_workers: int = 3
-@export var worker_limit: int = 2
+@export var worker_limit: int = 1
+
+@export_category("Inventory")
+@export var inv_input_name: String
+@export var inv_output_name: String
 
 var assigned_workers: Array = []
+var inventories: Dictionary[String, Inventory]
 @onready var build_info: Control = get_node("../../UI/BuildingInfo")
 
 
 func _ready() -> void:
-	add_inv("Input", 10)
-	add_inv("Output", 15)
+	add_inv(inv_input_name, 10)
+	add_inv(inv_output_name, 15)
 	var item = load("res://Planet-Section/Resources/Items/wood_log.tres")
 	var item_stack = ItemStack.new_stack(item, 10)
-	inventories["Input"].add_item_to_inv(item_stack)
+	inventories[inv_input_name].add_item_to_inv(item_stack)
 
 
 func inv_changed(inv: Inventory) -> void:
