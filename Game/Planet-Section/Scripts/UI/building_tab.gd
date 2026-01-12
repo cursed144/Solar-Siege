6b6d396ef2f4fbe791d8e37273037fdc390476e7
cursed144 <107793642+cursed144@users.noreply.github.com>
2026-01-12 13:57:05 +0200

    Added visual feedback for clicking the the building tabs in the builfing menu
    Updates the building tab to visually indicate when it is pressed. The tab now uses toggle mode, and script logic ensures only one tab is pressed at a time. Also the 'General' tab as pressed by default.

--- Game/Planet-Section/Scripts/UI/building_tab.gd
+++ Game/Planet-Section/Scripts/UI/building_tab.gd
@@ -11,4 +11,12 @@ func _ready() -> void:
 
 
 func _on_pressed() -> void:
+	if name != "Exit":
+		var tabs = get_parent()
+		for tab in tabs.get_children():
+			tab.button_pressed = false
+		button_pressed = true
+	else:
+		button_pressed = false
+	
 	tab_pressed.emit(name)
