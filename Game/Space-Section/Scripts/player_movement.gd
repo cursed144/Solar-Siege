8b021bcadc08be6f669c0fb9f87250b1683cf6e7
cursed144 <107793642+cursed144@users.noreply.github.com>
2026-03-06 12:10:49 +0200

    Fixed merge conflict + added soft cap on rotation

--- Game/Space-Section/Scripts/player_movement.gd
+++ Game/Space-Section/Scripts/player_movement.gd
@@ -1,4 +1,4 @@
-extends DamagableEntity
+extends Node
 
 var ROT_SPEED := 13.5
 var MAX_ROT_SPEED := 3.0
@@ -13,7 +13,13 @@ var LIN_BRAKE_BOOST := 15.0
 
 # how softly thrust fades near the cap (bigger = softer)
 var THRUST_SOFTNESS := 80.0
+var ROT_SOFTNESS := 1.0
 
+@onready var target: RigidBody2D = get_parent()
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("right_click"):
+		target.apply_torque_impulse(1000)
 
 func _physics_process(delta: float) -> void:
 	handle_movement(delta)
@@ -21,15 +27,15 @@ func _physics_process(delta: float) -> void:
 
 
 func handle_movement(delta: float) -> void:
-	var forward := Vector2.UP.rotated(rotation).normalized()
+	var forward := Vector2.UP.rotated(target.rotation).normalized()
 	
 	if Input.is_action_pressed("down"):
-		linear_velocity = linear_velocity.move_toward(Vector2.ZERO, LIN_DECAY * LIN_BRAKE_BOOST * delta)
+		target.linear_velocity = target.linear_velocity.move_toward(Vector2.ZERO, LIN_DECAY * LIN_BRAKE_BOOST * delta)
 		return
 	
 	if Input.is_action_pressed("up"):
 		# forward component of velocity (only what matters for "top speed")
-		var fwd_speed := linear_velocity.dot(forward)
+		var fwd_speed := target.linear_velocity.dot(forward)
 		
 		# Soft cap: scale thrust down as you approach the cap.
 		# - Below cap: scale ~1
@@ -39,19 +45,30 @@ func handle_movement(delta: float) -> void:
 		var thrust_scale := clampf(remaining / THRUST_SOFTNESS, 0.0, 1.0)
 		
 		if thrust_scale > 0.0:
-			apply_central_force(forward * LIN_SPEED * thrust_scale)
+			target.apply_central_force(forward * LIN_SPEED * thrust_scale)
 	else:
-		linear_velocity = linear_velocity.move_toward(Vector2.ZERO, LIN_DECAY * delta)
+		target.linear_velocity = target.linear_velocity.move_toward(Vector2.ZERO, LIN_DECAY * delta)
 
 
 func handle_rotation(delta: float) -> void:
-	if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
-		angular_velocity = move_toward(angular_velocity, 0.0, ROT_DECAY * ROT_STABILITY_BOOST * delta)
-	elif Input.is_action_pressed("right"):
-		apply_torque(ROT_SPEED)
-	elif Input.is_action_pressed("left"):
-		apply_torque(-ROT_SPEED)
-	else:
-		angular_velocity = move_toward(angular_velocity, 0.0, ROT_DECAY * delta)
+	var ang_vel := target.angular_velocity
 	
-	angular_velocity = clampf(angular_velocity, -MAX_ROT_SPEED, MAX_ROT_SPEED)
+	if Input.is_action_pressed("right") and Input.is_action_pressed("left"):
+		target.angular_velocity = move_toward(ang_vel, 0.0, ROT_DECAY * ROT_STABILITY_BOOST * delta)
+	
+	elif Input.is_action_pressed("right"):
+		var remaining := MAX_ROT_SPEED - ang_vel
+		var torque_scale := clampf(remaining / ROT_SOFTNESS, 0.0, 1.0)
+		
+		if torque_scale > 0.0:
+			target.apply_torque(ROT_SPEED * torque_scale)
+	
+	elif Input.is_action_pressed("left"):
+		var remaining := MAX_ROT_SPEED + ang_vel
+		var torque_scale := clampf(remaining / ROT_SOFTNESS, 0.0, 1.0)
+		
+		if torque_scale > 0.0:
+			target.apply_torque(-ROT_SPEED * torque_scale)
+	
+	else:
+		target.angular_velocity = move_toward(ang_vel, 0.0, ROT_DECAY * delta)
